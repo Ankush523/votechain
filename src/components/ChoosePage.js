6aa94cfb0db7d65fcdd63625f9fe8a989297f47a
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-07-15 18:52:55 +0530

    homepage design complete

--- src/components/ChoosePage.js
+++ src/components/ChoosePage.js
@@ -2,32 +2,31 @@ import React from 'react';
 const ChoosePage = () => {
     return (
         <div>
-            <label className='text-[40px] font-serif '>WELCOME TO INDIA'S FIRST</label>
+            <label className='text-[50px] font-serif text-purple-800'>WELCOME TO INDIA'S FIRST</label>
             <br/>
-            <label className='text-[40px] font-serif'>DECENTRALIZED VOTING PLATFORM</label>
+            <label className='text-[50px] font-serif text-purple-800'>DECENTRALIZED VOTING PLATFORM</label>
             <br/>
             <br/>
             <br/>
-            <p className='flex flex-row pl-[20px] text-[20px] text-left'>
-                Our Aim is to make India's Voting System : 
+            <div className='flex flex-row pl-[20px] text-[20px] text-left text-purple-500'>
+            <p >
+                <label className='text-[30px] text-purple-800'>Our Aim is to make India's Voting System :</label>
                 <br/>More secure
                 <br/>Transparent
                 <br/>Reliable and 
                 <br/>Immutable
             </p>
+            </div>
             <br/>
             <br/>
-            <p className='text-[20px] '>
-                VOTE FROM ANY TIME, ANYWHERE WITHOUT ANY HASSLE
+            <p className='text-[40px] text-purple-800'>
+                VOTE FROM ANYWHERE, ANY TIME WITHOUT ANY HASSLE
             </p>
             <br/>
             <br/>
-            <label className='text-[20px]'>Let's Get Started</label>
-            <br/>
-            <br/>
         <div className='flex flex-row w-[100vw] h-[fit-content] p-[20px] justify-around'>
-            <a href='/voter'><button className='text-[20px] w-[fit-content] h-[fit-content] rounded-md hover:shadow-xl p-[4px]'>Continue as Voter</button></a>
-            <a href='/candidate'><button className='text-[20px] w-[fit-content] h-[fit-content] rounded-md hover:shadow-xl p-[4px]'>Register as Candidate</button></a>
+            <a href='/voter'><button className='text-[20px] w-[fit-content] h-[fit-content] rounded-md hover:shadow-xl p-[8px] bg-purple-800 text-white'>Continue as Voter</button></a>
+            <a href='/candidate'><button className='text-[20px] w-[fit-content] h-[fit-content] rounded-md hover:shadow-xl p-[8px] bg-purple-800 text-white'>Register as Candidate</button></a>
         </div>
         </div>
     );
